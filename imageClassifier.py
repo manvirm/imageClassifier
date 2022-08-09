72ce70781193d94fbfdbe9f3d0028a958854d4fc
Manvir Mann <manvirm29@gmail.com>
2022-08-08 23:55:19 -0700

    define neural net structure

--- imageClassifier.py
+++ imageClassifier.py
@@ -17,8 +17,19 @@ fashion_mnist = keras.datasets.fashion_mnist
 # Returns 10k testing images in second set
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
-# Show data
+# Show data in terminal
 print(train_labels[0])
 print(train_images[0])
+
+# show image, grayscaled
 plt.imshow(train_images[0], cmap='gray', vmin=0, vmax=255)
 plt.show()
+
+# Define neural net structure
+model = keras.Sequential([
+    # input layer (compatible with dataset)
+    # one flat layer of our data
+    keras.layers.Flatten(input_shape=(28, 28))
+
+
+])
